e6bf85fc0da50abffe3af3da82151d747d186c48
dc004 <dc004@wangke.local>
2016-01-04 14:26:59 +0800

    Initial Commit

---
+++ Masonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry
+//
+//  Created by dc004 on 16/1/4.
+//  Copyright © 2016年 gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Masonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Masonry
+//
+//  Created by dc004 on 16/1/4.
+//  Copyright © 2016年 gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
